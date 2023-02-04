af31f61e396711f58a748e4aad3d49492a093e52
Ivan Meshkov <vanek.meshkoff@yandex.ru>
2023-02-04 09:41:20 +0300

    Ex_02 added

---
+++ Ex_02_YesSquare/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача №2: По двум заданным числам проверять, 
+//            является ли одно квадратом другого
+
+Console.Clear();
+Console.Write("Ввведите первое число: ");
+int number1 = int.Parse(Console.ReadLine() ?? "");
+Console.Write("Ввведите второе число: ");
+int number2 = int.Parse(Console.ReadLine() ?? "");
+if (number1 == number2*number2)
+{
+  Console.WriteLine($"Первое число {number1} является квадратом второго числа {number2}");
+}
+else if (number2 == number1*number1)
+{
+  Console.WriteLine($"Второе число {number2} является квадратом первого числа {number1}");
+}
+else
+{
+  Console.WriteLine("Ни одно из этих двух чисел не является квадратом второго");
+}
