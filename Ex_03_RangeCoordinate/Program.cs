9afac4f66dd9fde4fee70dad10b7ee601fa38fa1
Ivan Meshkov <vanek.meshkoff@yandex.ru>
2023-02-04 10:20:18 +0300

    Ex_03 added

---
+++ Ex_03_RangeCoordinate/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача №3: Задать номер четверти, показать диапазоны для возможных координат.
+Console.Clear();
+Console.Write("Введите номер четверти: ");
+int quarterNumber = int.Parse(Console.ReadLine() ?? "");
+switch (quarterNumber)
+{
+  case 1:
+    Console.WriteLine("Первая координатная четверть:");
+    Console.WriteLine("Диапазон координат по X: [0, +бесконечность)");
+    Console.WriteLine("Диапазон координат по Y: [0, +бесконечность)");
+    break;
+  case 2:
+    Console.WriteLine("Вторая координатная четверть:");
+    Console.WriteLine("Диапазон координат по X: [0, -бесконечность)");
+    Console.WriteLine("Диапазон координат по Y: [0, +бесконечность)");
+    break;
+  case 3:
+    Console.WriteLine("Третья координатная четверть:");
+    Console.WriteLine("Диапазон координат по X: [0, -бесконечность)");
+    Console.WriteLine("Диапазон координат по Y: [0, -бесконечность)");
+    break;
+  case 4:
+    Console.WriteLine("Четвертая координатная четверть:");
+    Console.WriteLine("Диапазон координат по X: [0, +бесконечность)");
+    Console.WriteLine("Диапазон координат по Y: [0, -бесконечность)");
+    break;
+  default:
+    Console.WriteLine("Такой координатной четверти не существует!");
+    break;
+}
